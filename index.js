f20e7fdc41ccb39a332ef95c392cc46a47b122e6
kumaran26 <32246188+kumaran26@users.noreply.github.com>
2024-09-22 17:48:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Node Server is running!! 🌟');
+    res.send('Node Server is running!!');
 });
 
 app.listen(3000, () => {
