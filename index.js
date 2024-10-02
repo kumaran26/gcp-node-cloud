6bbd50ad3cacd3dc96e28ec469d9a780629a711c
kumaran26 <32246188+kumaran26@users.noreply.github.com>
2024-10-02 13:44:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const pg = require('pg');
 
+const config = {
+  connectionString: 'postgres://postgres:postgres@35.226.224.213/test'
+};
+
+const pool = new pg.Pool(config);
+
+const DB = {
+    query: function(query, callback) {
+        pool.connect((err, client, done) => {
+            if(err) return callback(err)
+            client.query(query, (err, results) => {
+                done()
+                if(err) { console.error("ERROR: ", err) }
+                if(err) { return callback(err) }
+                callback(null, results.rows)
+            })
+        });
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Node Server is running!!');
