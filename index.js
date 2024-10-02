5bfa23f9e0d9547888d6907b91ff154b14f62461
kumaran26 <32246188+kumaran26@users.noreply.github.com>
2024-10-02 13:36:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const { Pool } = require('pg');
 
-var config = {
-    user: 'postgres', 
-    database: 'test', 
-    password: 'postgres', 
-    host: '35.226.224.213', 
-    port: 5432, 
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000
-};
-const pool = new Pool(config);
-pool.on('error', function (err, client) {
-    console.error('idle client error', err.message, err.stack);
-});
-pool.query('SELECT $1::int AS number', ['2'], function(err, res) {
-    if(err) {
-        return console.error('error running query', err);
-    }
-    console.log('number:', res.rows[0].number);
-});
 
 app.get('/', (req, res) => {
     res.send('Node Server is running!!');
