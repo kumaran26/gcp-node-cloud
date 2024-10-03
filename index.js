444f39e96c717574a54089139eadcfd33611210c
kumaran26 <32246188+kumaran26@users.noreply.github.com>
2024-10-03 17:27:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,38 @@ const client = new Client({
 
 let hello = "";
 
-client.connect()
-    .then(() => {
-        hello = "succcess"
-         client.query('SELECT * FROM persons').then((resp) => {
-             hello = resp;
-         });
-        console.log('Connected to PostgreSQL!')
+async function queryDatabase() {
+    try {
+        // Connect to the database
+        await client.connect();
+        console.log('Connected to PostgreSQL!');
+
+        // Example query: select all rows from a table named 'users'
+        const res = await client.query('SELECT * FROM users');
+        console.log('Query Results:', res.rows); // Logs the result rows
+        hello = res.rows;
+    } catch (err) {
+        console.error('Error executing query', err.stack);
+    } finally {
+        // Disconnect from the database
+        await client.end();
+        console.log('Disconnected from PostgreSQL');
+    }
+}
+
+queryDatabase()
+
+// client.connect()
+//     .then(() => {
+//         hello = "succcess"
+//          client.query('SELECT * FROM persons').then((resp) => {
+//              hello = resp;
+//          });
+//         console.log('Connected to PostgreSQL!')
                 
-                })
-    .catch(err => console.error('Connection error', err.stack))
-    .finally(() => client.end());
+//                 })
+//     .catch(err => console.error('Connection error', err.stack))
+//     .finally(() => client.end());
 
 app.get('/', (req, res) => {
     res.send(`Node Server is running!! ${hello}`);
